718f1632627cc3cf013489e14fc9e57a4ec2005e
vrashal <vrashaltaribagil@gmail.com>
2026-03-22 11:44:06 -0700

    Added more Jest tests

--- sum.test.js
+++ sum.test.js
@@ -3,3 +3,19 @@ const sum = require('./sum');
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
 });
+
+test('adds -1 + 1 to equal 0', () => {
+    expect(sum(-1, 1)).toBe(0);
+});
+
+test('adds 0 + 0 to equal 0', () => {
+    expect(sum(0, 0)).toBe(0);
+});
+
+test('adds 2.5 + 3.5 to equal 6', () => {
+    expect(sum(2.5, 3.5)).toBe(6);
+});
+
+test('adds -2 + -3 to equal -5', () => {
+    expect(sum(-2, -3)).toBe(-5);
+});
