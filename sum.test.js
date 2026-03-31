5f633a5b8d60c9fbb94a58502f5f57b857093220
vrashal <vrashaltaribagil@gmail.com>
2026-03-31 00:31:10 -0700

    docker file and test script

--- sum.test.js
+++ sum.test.js
@@ -15,7 +15,3 @@ test('adds 0 + 0 to equal 0', () => {
 test('adds 2.5 + 3.5 to equal 6', () => {
     expect(sum(2.5, 3.5)).toBe(6);
 });
-
-test('adds -2 + -3 to equal -5', () => {
-    expect(sum(-2, -3)).toBe(-5);
-});
